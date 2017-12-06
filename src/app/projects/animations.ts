f459da96a377803d15fa447eca86bdcc6784ed25
Wolfgang Becker <wolli.becker@gmail.com>
2017-12-06 22:51:25 +0100

    Animate transitions when a project gets marked

--- src/app/projects/animations.ts
+++ src/app/projects/animations.ts
@@ -3,11 +3,29 @@ import { trigger, style, state, transition, animate } from '@angular/animations'
 export const markedTrigger = trigger('markedState', [
   state('default', style({
     border: '1px solid black',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
+    padding: '20px'
   })),
   state('marked', style({
     border: '2px solid blue',
-    backgroundColor: '#caeff9'
+    backgroundColor: '#caeff9',
+    padding: '19px'
   })),
-  transition('default => marked', animate('1200ms ease-out'))
+  transition('default => marked', [
+    style({
+      border: '2px solid black',
+      padding: '19px'
+    }),
+    animate('200ms ease-out', style({
+      transform: 'scale(1.05)'
+    })),
+    animate(200)
+  ]),
+  transition('marked => default', [
+    style({
+      border: '1px solid blue',
+      padding: '20px'
+    }),
+    animate('300ms ease-out')
+  ])
 ]);
