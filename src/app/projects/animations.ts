4edb6759c9690d24c74ef3e543a8946a5e9fab92
Wolfgang Becker <wolli.becker@gmail.com>
2017-12-10 18:07:45 +0100

    Change project list animation to make projects fly in

--- src/app/projects/animations.ts
+++ src/app/projects/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, style, state, transition, animate } from '@angular/animations';
+import { trigger, style, state, transition, animate, keyframes } from '@angular/animations';
 
 export const markedTrigger = trigger('markedState', [
   state('default', style({
@@ -32,14 +32,38 @@ export const markedTrigger = trigger('markedState', [
 
 export const newProjectTrigger = trigger('newProjectState', [
   transition(':enter', [
-    style({
-      opacity: 0
-    }),
-    animate(300)
+    animate('500ms ease-out', keyframes([
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)',
+        offset: 0
+      }),
+      style({
+        opacity: 1,
+        transform: 'translateX(15%)',
+        offset: 0.4
+      }),
+      style({
+        opacity: 1,
+        transform: 'translateX(0)',
+        offset: 1
+      }),
+    ]))
   ]),
   transition(':leave', [
-    animate(300, style({
-      opacity: 0
-    }))
+    animate('500ms ease-in', keyframes([
+      style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      }),
+      style({
+        opacity: 1,
+        transform: 'translateX(-15%)'
+      }),
+      style({
+        opacity: 1,
+        transform: 'translateX(100%)'
+      })
+    ]))
   ])
 ]);
